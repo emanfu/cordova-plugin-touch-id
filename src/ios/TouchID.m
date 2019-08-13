c2c1436aab38d83fe3192f6520476b0770b9eeb2
Eman Fu <emanfu@gmail.com>
2019-08-13 10:22:39 -0700

    Fixed the problem that fallback to passcode didn't work.

--- src/ios/TouchID.m
+++ src/ios/TouchID.m
@@ -21,7 +21,7 @@ NSString *keychainItemServiceName;
     NSError *error = nil;
     LAContext *laContext = [[LAContext alloc] init];
 
-    if ([laContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+    if ([laContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:&error]) {
       NSString *biometryType = @"touch";
       if (@available(iOS 11.0, *)) {
         if (laContext.biometryType == LABiometryTypeFaceID) {
@@ -84,47 +84,32 @@ NSString *keychainItemServiceName;
   NSString *callbackId = command.callbackId;
 
   [self.commandDelegate runInBackground:^{
-
-    if (keychainItemServiceName == nil) {
-      NSString *bundleID = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"];
-      keychainItemServiceName = [bundleID stringByAppendingString:@".TouchIDPlugin"];
-    }
-
-    if (![self createKeyChainEntry]) {
-      NSLog(@"Keychain trouble. Falling back to verifyFingerprintWithCustomPasswordFallback.");
-      [self verifyFingerprintWithCustomPasswordFallback:command];
-      return;
-    }
-
-    // Create the keychain query attributes using the values from the first part of the code.
-    NSMutableDictionary * query = [[NSMutableDictionary alloc] initWithObjectsAndKeys:
-                                   (__bridge id)(kSecClassGenericPassword), kSecClass,
-                                   keychainItemIdentifier, kSecAttrAccount,
-                                   keychainItemServiceName, kSecAttrService,
-                                   message, kSecUseOperationPrompt,
-                                   nil];
-
-    // Start the query and the fingerprint scan and/or device passcode validation
-    OSStatus userPresenceStatus = SecItemCopyMatching((__bridge CFDictionaryRef)query, NULL);
-
-    // Ignore the found content of the key chain entry (the dummy password) and only evaluate the return code.
-    if (noErr == userPresenceStatus)
-    {
-      NSLog(@"Fingerprint or device passcode validated.");
-      [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK]
-                                  callbackId:command.callbackId];
-    }
-    else
-    {
-      NSLog(@"Fingerprint or device passcode could not be validated. Status %d.", (int) userPresenceStatus);
-
-      NSError *error = [NSError errorWithDomain:NSOSStatusErrorDomain code:userPresenceStatus userInfo:nil];
-      NSArray *errorKeys = @[@"code", @"localizedDescription"];
-      [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-                                                           messageAsDictionary:[error dictionaryWithValuesForKeys:errorKeys]]
-                                  callbackId:callbackId];
-      return;
-    }
+      NSError *error = nil;
+      LAContext *laContext = [[LAContext alloc] init];
+      
+      if (![laContext canEvaluatePolicy:LAPolicyDeviceOwnerAuthentication error:&error]) {
+          [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[error localizedDescription]]
+                                      callbackId:callbackId];
+          return;
+      }
+      
+      // if we add a 'verifyFingerprintWithOptions' method we can add stuff like this:
+      // the nr of seconds you allow to reuse the last touchid device unlock (default 0, so never reuse)
+      //    laContext.touchIDAuthenticationAllowableReuseDuration = 30;
+      
+      [laContext evaluatePolicy:LAPolicyDeviceOwnerAuthentication localizedReason:message reply:^(BOOL authOK, NSError *error) {
+          if (authOK) {
+              [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK]
+                                          callbackId:callbackId];
+          } else {
+              NSLog(@"Fingerprint or device passcode could not be validated.");
+              // invoked when the scan failed 3 times in a row, the cancel button was pressed, or the 'enter password' button was pressed
+              NSArray *errorKeys = @[@"code", @"localizedDescription"];
+              [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                                   messageAsDictionary:[error dictionaryWithValuesForKeys:errorKeys]]
+                                          callbackId:callbackId];
+          }
+      }];
   }];
 }
 
@@ -180,35 +165,6 @@ NSString *keychainItemServiceName;
       }
     }];
   }];
-}
-
-// Note that this needs to run only once but it can deal with multiple runs
-- (BOOL) createKeyChainEntry {
-  NSMutableDictionary	* attributes = [[NSMutableDictionary alloc] initWithObjectsAndKeys:
-                                      (__bridge id)(kSecClassGenericPassword), kSecClass,
-                                      keychainItemIdentifier, kSecAttrAccount,
-                                      keychainItemServiceName, kSecAttrService,
-                                      nil];
-
-  CFErrorRef accessControlError = NULL;
-  SecAccessControlRef accessControlRef = SecAccessControlCreateWithFlags(
-                                                                         kCFAllocatorDefault,
-                                                                         kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
-                                                                         2, // either kSecAccessControlBiometryAny (iOS 11.3+) or kSecAccessControlTouchIDAny (iOS < 11.3),
-                                                                         &accessControlError);
-  if (accessControlRef == NULL || accessControlError != NULL)
-  {
-    NSLog(@"Can't store identifier '%@' in the KeyChain: %@.", keychainItemIdentifier, accessControlError);
-    return NO;
-  }
-
-  attributes[(__bridge id)kSecAttrAccessControl] = (__bridge id)accessControlRef;
-  attributes[(__bridge id)kSecUseAuthenticationUI] = @YES;
-  // The content of the password is not important.
-  attributes[(__bridge id)kSecValueData] = [@"dummy content" dataUsingEncoding:NSUTF8StringEncoding];
-
-  SecItemAdd((__bridge CFDictionaryRef)attributes, NULL);
-  return YES;
 }
 
 @end
